b59854f178364baa9c02a100af710d40c2560cb8
fmf1381 <fahimfuladi@gmail.com>
2025-07-31 08:17:59 +0330

    add modal for news details

--- admin-panel/pages/news/index.php
+++ admin-panel/pages/news/index.php
@@ -1,7 +1,11 @@
 <?php
 include "../../include/layout/header.php";
 
-$news = $db->query("SELECT news.id,news.title,news.image,news.description,news.status, user.name AS reporter_name FROM news INNER JOIN user ON news.reporter_id = user.id ORDER BY id DESC");
+$news = $db->query("SELECT news.id,news.title,news.image,news.description,news.status, user.name AS reporter_name ,category.name AS category_name
+FROM news 
+INNER JOIN user ON news.reporter_id = user.id 
+INNER JOIN category ON news.category_id = category.id
+ORDER BY id DESC");
 
 if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
     $id = $_GET['id'];
@@ -33,7 +37,6 @@ if (isset($_GET['status']) && isset($_GET['id'])) {
     exit();
     }
 }
-
 ?>
 
 <div class="container-fluid">
@@ -72,6 +75,18 @@ if (isset($_GET['status']) && isset($_GET['id'])) {
                                         <td><?= $news_1['title'] ?></td>
                                         <td><?= $news_1['reporter_name'] ?></td>
                                         <td>
+                                        <button 
+                                            class="btn btn-sm btn-outline-primary"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#newsModal"
+                                            data-title="<?= htmlspecialchars($news_1['title']) ?>"
+                                            data-image="/news-agency-website/assets/images/<?= $news_1['image'] ?>"
+                                            data-description="<?= htmlspecialchars($news_1['description']) ?>"
+                                            data-reporter="<?= htmlspecialchars($news_1['reporter_name']) ?>"
+                                            data-category="<?= htmlspecialchars($news_1['category_name']) ?>"
+                                            >
+                                            جزئیات
+                                        </button>
                                         <?php if ($news_1['status'] == 'confirmed') : ?>
                                                 <button class="btn btn-sm btn-outline-dark disabled">تایید شده</button>
                                             <?php elseif($news_1['status'] == 'pending') : ?>
@@ -99,9 +114,56 @@ if (isset($_GET['status']) && isset($_GET['id'])) {
     </div>
 </div>
 
+<div class="modal fade" id="newsModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-body p-4">
+        <div class="card border-0">
+          <img id="modalImage" src="" class="card-img-top mb-3" alt="post-image" />
+          <div class="card-body">
+            <div class="d-flex justify-content-between">
+              <h5 id="modalTitle" class="card-title fw-bold"></h5>
+              <div>
+                <span id="modalCategory" class="badge text-bg-secondary"></span>
+              </div>
+            </div>
+            <p id="modalDescription" class="card-text text-secondary pt-3"></p>
+            <div>
+              <p id="modalReporter" class="fs-6 mt-5 mb-0"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer border-0">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">بستن</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php
 include "../../include/layout/footer.php"
 ?>
+
+<script>
+  const newsModal = document.getElementById('newsModal');
+  newsModal.addEventListener('show.bs.modal', function (event) {
+    const button = event.relatedTarget;
+
+    const title = button.getAttribute('data-title');
+    const image = button.getAttribute('data-image');
+    const description = button.getAttribute('data-description');
+    const reporter = button.getAttribute('data-reporter');
+    const category = button.getAttribute('data-category');
+
+    document.getElementById('modalTitle').textContent = title;
+    document.getElementById('modalImage').src = image;
+    document.getElementById('modalDescription').textContent = description;
+    document.getElementById('modalReporter').textContent = "خبرنگار: " + reporter;
+    document.getElementById('modalCategory').textContent = category;
+  });
+</script>
+
 
 <script>
   function confirmDelete() {
